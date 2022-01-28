5e9592ff8f78822f341e88f05db85b06d6693fc7
Pelaez <paulopelaez@hotmail.com>
2022-01-27 22:43:03 -0300

    feath(auth) : implement jwt login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,12 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Request,
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
@@ -15,8 +17,9 @@ export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  signIn() {
-    return 'SignIn is success';
-    //return this.authService.login();
+  signIn(@Request() req: AuthRequest) {
+    //console.log(req.user);
+    //return 'SignIn is success';
+    return this.authService.login(req.user);
   }
 }
